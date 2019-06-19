e38d3cd077cfe0e306946bd7cff420ddd583fb71
jedokaplan <jed.kaplan@arve-research.com>
2019-06-19 14:59:30 +0800

    added source files for weathergenmod and randomdistmod

--- src/parametersmod.f90
+++ src/parametersmod.f90
@@ -6,15 +6,13 @@ use iso_fortran_env, only : int8,int16,int32,real32,real64,output_unit
 
 implicit none
 
-integer, parameter :: dp = real64      ! 8 byte real
-integer, parameter :: sp = real32      ! 4 byte real
-integer, parameter :: i2 = int16       ! 2 byte integer
-integer, parameter :: i4 = int32       ! 4 byte integer
+integer, parameter :: dp = real64       ! 8 byte real
+integer, parameter :: sp = real32       ! 4 byte real
+integer, parameter :: i2 = int16        ! 2 byte integer
+integer, parameter :: i4 = int32        ! 4 byte integer
 
-integer, parameter :: so = output_unit  !unit number for standard output
+integer, parameter :: so = output_unit  ! unit number for standard output
 
-integer, parameter, dimension(12) :: ndaymonth = (/ 31,28,31,30,31,30,31,31,30,31,30,31 /) ! number of days in each month
-
-real(sp), parameter :: Tfreeze = 273.15      ! freezing temperature of freshwater (K)
+real(sp), parameter :: Tfreeze = 273.15 ! freezing temperature of freshwater (K)
 
 end module parametersmod
